8f1dd649748d5e9afde91dae374d894380f87055
filkur <filipkuranda@gmail.com>
2021-03-13 22:39:33 +0100

    add show category function

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/category", name="category")
+ * Class CategoryController
+ * @package App\Controller
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route ("/show/{id}", name="show")
+     * @param Task $task
+     * @return Response
+     */
+    public function show (Task $task){
+
+        //create the show view
+        return $this-> render('post/show.html.twig', [
+            'task' => $task
+        ]);
+    }
+
+}
