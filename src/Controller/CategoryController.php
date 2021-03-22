32e507c1ea240580510212e886eafbd39896b083
filkur <filipkuranda@gmail.com>
2021-03-22 16:31:00 +0100

    add create category method

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,9 +6,13 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Task;
 use App\Entity\User;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,6 +37,39 @@ class CategoryController extends AbstractController
                 'categories' => $categories,
             ]
         );
+    }
+
+    /**
+     * @Route("/create", name="create_category")
+     */
+    public function create(Request $request):Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //entity manager
+            $em = $this->getDoctrine()
+                       ->getManager()
+            ;
+            $category->setCreatedAt(new \DateTime());
+            $category->setUser($this->getUser());
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('category_index'));
+        }
+
+        return $this->render(
+            'category/create.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+
     }
 
     /**
