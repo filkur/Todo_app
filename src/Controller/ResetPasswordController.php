0f2b7cc4263daeba6f92ea2639fa651d44ff6bec
filkur <filipkuranda@gmail.com>
2021-03-30 20:57:05 +0200

    modify email sender address and route after change password

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -122,7 +122,7 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('main');
         }
 
         return $this->render('reset_password/reset.html.twig', [
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('filip.kurandadev@gmail.com', 'Admin Mail Bot'))
+            ->from(new Address('todoapppoland@gmail.com', 'Admin Mail Bot'))
             ->to($user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
