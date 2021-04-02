878d96bb0fe6f91846c4ae905cc6fd72413197a0
filkur <filipkuranda@gmail.com>
2021-04-02 19:05:16 +0200

    show alert when password is incorrect

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -56,6 +56,9 @@ class UserController extends AbstractController
                     )
                 );
             }
+            else{
+                $this->addFlash('error', 'Wrong Password!');
+            }
 
         }
 
